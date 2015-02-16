188fbf082003e08efa7ce0b198b5bc28c0e6b7aa
Kenny <kroffo@oswego.edu>
2015-02-16 08:38:36 -0500

    Fixed some syntactical errors in my board.

--- board.js
+++ board.js
@@ -1,19 +1,19 @@
-colors = ["Red", "Blue", "Yellow", "Green", "Purple"]
+var colors = ["Red", "Blue", "Yellow", "Green", "Purple"]
 
-function shell(attributes) = {
+function shell (attributes) = {
     this.color = attributes[0];
     this.level = attributes[1]; //We can define attributes or some other way of assigning these values.
-}
+};
 
-shells = []
+var shells = [];
 
 for (var i = 0; i < 4; i++) {
-    shells[i] = []
+    shells[i] = [];
     for (var k = 0; i < 4; i++) {
-	shells[i[k]] = shell([ Math.floor(Math.random()*5) , 1]);
+	shells[i[k]] = new shell([ Math.floor(Math.random()*5) , 1]);
     };
 };
 
 for (var i = 0; i < 4; i = i + 4) {
-    console.log("| " + shells[i[0]].color + " " + shells[i[1]].color + " " + shells[i[2]].color + " "+ shells[i[3]].color + " |"
-}
+    console.log("| " + shells[i[0]].color + " " + shells[i[1]].color + " " + shells[i[2]].color + " "+ shells[i[3]].color + " |";
+};
